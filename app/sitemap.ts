121be1cc5569a72cbdfc2fa6f10e59008dca7259
ViniciusM15 <142498596+ViniciusM15IFC@users.noreply.github.com>
2026-09-21 14:56:14 -0300

    Update sitemap URL to remove '/sitemap'

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://jvmoveis.netlify.app/sitemap",
+      url: "https://jvmoveis.netlify.app",
       lastModified: new Date(),
     },
   ];
