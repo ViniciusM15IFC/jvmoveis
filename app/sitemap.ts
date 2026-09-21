51347a9bfdfc19a4e7de5ef7aed777fdac03ff26
ViniciusM15 <142498596+ViniciusM15IFC@users.noreply.github.com>
2026-09-21 14:52:46 -0300

    Update sitemap URL to include '/sitemap'

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://jvmoveis.netlify.app",
+      url: "https://jvmoveis.netlify.app/sitemap",
       lastModified: new Date(),
     },
   ];
